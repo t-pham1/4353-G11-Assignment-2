6f30b76e20aae1eccc3f57eb0c7362ff96eceb00
Christine Do <102181038+thechristinedo@users.noreply.github.com>
2023-10-10 04:10:55 -0500

    added post and get request

--- app.py
+++ app.py
@@ -1,21 +1,43 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash
 
 app = Flask(__name__)
+app.secret_key = "5473895728547392"
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
     return render_template('login.html')
 
-@app.route('/profile')
+@app.route('/profile', methods = ['GET', 'POST'])
 def profile():
+    if request.method == 'POST':
+        fullName = request.form.get('fullName')
+        addressOne = request.form.get('addressOne')
+        addressTwo = request.form.get('addressTwo')
+        city = request.form.get('city')
+        state = request.form.get('state')
+        zipcode = request.form.get('zipcode')
+        # print(fullName + ' ' + addressOne + ' ' + addressTwo + ' ' + city + ' ' + state + ' ' + zipcode)
+
     return render_template('profile.html')
 
-@app.route('/sign_up')
+@app.route('/sign_up', methods = ['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        # print(username + ' ' + password)
+
+        # if len(username) < 1:
+        #     flash('Please enter a username.', category='error')
+        # elif len(password) < 1:
+        #     flash('Please enter a password.', category='error') 
+        # else:
+        #     flash('You have successfully registered.', category='sucess')
+        
     return render_template('sign_up.html')
 
 @app.route('/quote')
